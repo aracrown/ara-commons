5b3eafe83e9212a694d924d9e4cc584b1d0decf3
Vytautas R. <turisto@gmail.com>
2014-06-07 23:03:59 +0300

    minor updates

---
+++ ara-commons/src/main/java/org/aracrown/commons/date/GMT.java
@@ -0,0 +1,28 @@
+package org.aracrown.commons.date;
+
+public class GMT {
+	public static final String GMTM1000 = "GMT-10:00";
+	public static final String GMTM0900 = "GMT-09:00";
+	public static final String GMTM0800 = "GMT-08:00";
+	public static final String GMTM0700 = "GMT-07:00";
+	public static final String GMTM0600 = "GMT-06:00";
+	public static final String GMTM0500 = "GMT-05:00";
+	public static final String GMTM0400 = "GMT-04:00";
+	public static final String GMTM0300 = "GMT-03:00";
+	public static final String GMTM0200 = "GMT-02:00";
+	public static final String GMTM0100 = "GMT-01:00";
+	public static final String GMTP0000 = "GMT+00:00";
+	
+	public static final String GMTP0100 = "GMT+01:00";
+	public static final String GMTP0200 = "GMT+02:00";
+	public static final String GMTP0300 = "GMT+03:00";
+	public static final String GMTP0400 = "GMT+04:00";
+	public static final String GMTP0500 = "GMT+05:00";
+	public static final String GMTP0600 = "GMT+06:00";
+	public static final String GMTP0700 = "GMT+07:00";
+	public static final String GMTP0800 = "GMT+08:00";
+	public static final String GMTP1000 = "GMT+10:00";
+	public static final String GMTP1100 = "GMT+11:00";
+	public static final String GMTP1200 = "GMT+12:00";
+	public static final String GMTP1300 = "GMT+13:00";
+}
