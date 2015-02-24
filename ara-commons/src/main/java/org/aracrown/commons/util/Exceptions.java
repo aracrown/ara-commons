07315213a893b7bec5e0152ba3edb8aaebf9bbeb
Vytautas R. <turisto@gmail.com>
2015-02-24 23:38:08 +0200

    google captcha added.

--- ara-commons/src/main/java/org/aracrown/commons/util/Exceptions.java
+++ ara-commons/src/main/java/org/aracrown/commons/util/Exceptions.java
@@ -46,14 +46,21 @@ public final class Exceptions {
 	}
 
 	/**
-	 * Checks {@code throwable} for a cause of type {@code class1}. Throws new exception if found.
+	 * Checks {@code throwable} for a cause of type {@code class1}. Throws new
+	 * exception if found.
 	 *
-	 * @param throwable         exception to check for cause type
-	 * @param class1            cause type to check
-	 * @param replacedThrowable new custom exception to throw if cause type was found
-	 * @param <X>               exception type to search for
-	 * @param <Y>               exception type to be re-thrown
-	 * @throws Y exception thrown if cause type was found in chain
+	 * @param throwable
+	 *            exception to check for cause type
+	 * @param class1
+	 *            cause type to check
+	 * @param replacedThrowable
+	 *            new custom exception to throw if cause type was found
+	 * @param <X>
+	 *            exception type to search for
+	 * @param <Y>
+	 *            exception type to be re-thrown
+	 * @throws Y
+	 *             exception thrown if cause type was found in chain
 	 */
 	public <X extends Throwable, Y extends Throwable> void throwNewIfContains(Throwable throwable, Class<X> class1, Y replacedThrowable) throws Y {
 		Iterable<X> i = Iterables.filter(Throwables.getCausalChain(throwable), class1);
@@ -64,12 +71,36 @@ public final class Exceptions {
 	}
 
 	/**
-	 * Checks {@code throwable} for a cause of type {@code class1}. Throws cause exception if found.
+	 * Checks {@code throwable} for a cause of type {@code class1}.
 	 *
-	 * @param throwable exception to check for cause type
-	 * @param class1    cause type to check
-	 * @param <X>       exception type to be re-thrown
-	 * @throws X exception thrown if cause type was found in chain
+	 * @param throwable
+	 *            exception to check for cause type
+	 * @param class1
+	 *            cause type to check
+	 * @param <X>
+	 *            exception type to search for
+	 */
+	public <X extends Throwable> boolean contains(Throwable throwable, Class<X> class1) {
+		Iterable<X> i = Iterables.filter(Throwables.getCausalChain(throwable), class1);
+		// Do not expect null here.
+		if (i.iterator().hasNext()) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks {@code throwable} for a cause of type {@code class1}. Throws cause
+	 * exception if found.
+	 *
+	 * @param throwable
+	 *            exception to check for cause type
+	 * @param class1
+	 *            cause type to check
+	 * @param <X>
+	 *            exception type to be re-thrown
+	 * @throws X
+	 *             exception thrown if cause type was found in chain
 	 */
 	public <X extends Throwable> void propagate(Throwable throwable, Class<X> class1) throws X {
 		Iterable<X> i = Iterables.filter(Throwables.getCausalChain(throwable), class1);
